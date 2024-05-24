cea964e3a9daf76a6e56e0510ff65f24bfd17e59
Malay Singh <malay4singh@gmail.com>
2024-05-24 10:39:39 +0530

    fixed responsiveness of project details

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -20,7 +20,8 @@ function ProjectDetails(props: ProjectDetailsProps){
         }
 
         return(
-                <div className={`p-7 border-2 border-black absolute w-[75%] h-[30%] ${props.visibility ? "visible" : "invisible"} bg-[#156064] text-[#FFFFF0] rounded-lg`}>
+                <div className={`p-7 border-2 border-black absolute w-[75%] h-[30%] ${props.visibility ? "visible" : "invisible"} bg-[#156064] text-[#FFFFF0] rounded-lg max-[600px]:w-[25%] max-[600px]:h-[89%] max-[600px]:overflow-y-scroll`}>
+                        
                         <div className="text-2xl ">
                                 {props.projectDetails.name}
                         </div>
@@ -35,7 +36,7 @@ function ProjectDetails(props: ProjectDetailsProps){
                                         </ul>
                                 </div>
 
-                                <img className="w-[420px] h-[200px] mr-9 object-cover" src={props.projectDetails.img} />
+                                <img className="w-[420px] h-[200px] mr-9 object-cover max-[600px]:hidden" src={props.projectDetails.img} />
                         </div>
                         
 
@@ -48,17 +49,18 @@ function ProjectDetails(props: ProjectDetailsProps){
                                 </ul>
                         </div>
                        
-
-                        {props.projectDetails.name !== "My Portfolio" &&
-                                <div className="border-2 border-black text-black bg-[#FFFFF0] px-1 rounded-md absolute bottom-[30px] right-[340px] min-[1700px]:right-[390px] hover:cursor-pointer transition ease-in-out hover:shadow-2xl hover:scale-110 duration-400">
-                                        <a href={props.projectDetails.link}>Open</a>
+                        <div className="max-[600px]:flex max-[600px]:mt-6 max-[600px]:justify-between">
+                                {props.projectDetails.name !== "My Portfolio" &&
+                                        <div className="border-2 border-black text-black bg-[#FFFFF0] px-1 rounded-md absolute bottom-[30px] right-[340px] min-[1700px]:right-[390px] hover:cursor-pointer transition ease-in-out hover:shadow-2xl hover:scale-110 duration-400 max-[600px]:static max-[600px]:w-fit">
+                                                <a href={props.projectDetails.link}>Open</a>
+                                        </div>
+                                }
+                                <div className="border-2 border-black text-black bg-[#FFFFF0] px-1 rounded-md absolute bottom-[30px] right-[150px] min-[1700px]:right-[160px] hover:cursor-pointer transition ease-in-out hover:shadow-2xl hover:scale-110 duration-400 max-[600px]:right-[105px] max-[600px]:static max-[600px]:w-fit">
+                                        <a href={props.projectDetails.code} target="_blank">Source Code</a>
                                 </div>
-                        }
-                        <div className="border-2 border-black text-black bg-[#FFFFF0] px-1 rounded-md absolute bottom-[30px] right-[150px] min-[1700px]:right-[160px] hover:cursor-pointer transition ease-in-out hover:shadow-2xl hover:scale-110 duration-400">
-                                <a href={props.projectDetails.code} target="_blank">Source Code</a>
-                        </div>
-                        <div onClick={hideDetails} className="border-2 border-black text-black bg-[#FFFFF0] px-1 rounded-md absolute bottom-[30px] right-[40px] hover:cursor-pointer transition ease-in-out hover:shadow-2xl hover:scale-110 duration-400">
-                                Close
+                                <div onClick={hideDetails} className="border-2 border-black text-black bg-[#FFFFF0] px-1 rounded-md absolute bottom-[30px] right-[40px] hover:cursor-pointer transition ease-in-out hover:shadow-2xl hover:scale-110 duration-400 max-[600px]:right-[30px] max-[600px]:static max-[600px]:w-fit">
+                                        Close
+                                </div>
                         </div>
                 </div>
         )
