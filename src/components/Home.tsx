f6798cd36379ea61e1abfe89d4250a0aeea945e5
Malay Singh <malay4singh@gmail.com>
2024-08-10 17:31:59 +0530

    updated resume link

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -26,7 +26,7 @@ function Home(){
                                         <div className="h-[100%]">
                                                 <a href="https://www.linkedin.com/in/malay-singh-15b722274/" target="_blank"><img className="h-[100%]" src="linkedin.svg" /></a>
                                         </div>
-                                        <a className="h-full flex items-center" href="https://drive.google.com/file/d/15esulUdejfbUMK6iIHqQJPGsBVbTkBR8/view?usp=sharing" target="_blank">
+                                        <a className="h-full flex items-center" href="https://drive.google.com/file/d/1dgEhTQrIaLZYp-kIb_gYg-tiGKCOqGaC/view?usp=sharing" target="_blank">
                                                 <div className="px-1 rounded bg-[#156064] text-[#fffff0] flex gap-2 h-[90%] items-center">
                                                         RESUME <img src="link.svg" width={'20px'} />
                                                 </div>
