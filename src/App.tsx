76a098b4fe19efefc567635923f94010a5dc39d0
Malay Singh <malay4singh@gmail.com>
2024-12-13 13:58:53 +0530

    fixed transitions and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Container from "./components/Container";
 import About from "./components/About";
 import Home from "./components/Home";
 import WorkExperience from "./components/WorkExperience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-// import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { useEffect, useState } from "react";
 
 function App() {
 
-        // const location = useLocation();
+        const location = useLocation();
 
-        // const [previousPage, setPreviousPage] = useState<number>(0);
+        const [previousPage, setPreviousPage] = useState<number>(0);
         const [currentPage, setCurrentPage] = useState<number>(1);
 
-        // const isSmallScreen = window.innerWidth < 601;
+        const isSmallScreen = window.innerWidth < 601;
 
         useEffect( () => {
                 const setPage = () => {
-                        // setPreviousPage(currentPage);
+                        setPreviousPage(currentPage);
                 }
 
                 setPage();
@@ -31,7 +31,7 @@ function App() {
 
                         <Route path="/" element={<Container setCurrentPage={setCurrentPage} />}>
 
-                                {/* <Route path="*" element={<TransitionGroup>
+                                <Route path="*" element={<TransitionGroup>
                                         <CSSTransition key={location.key} timeout={500} classNames={ isSmallScreen ? ( previousPage < currentPage ? "left" : "right" ) : ( previousPage < currentPage ? "up" : "down" ) }>
 
                                                 <Routes location={location}>
@@ -43,13 +43,7 @@ function App() {
                                                 </Routes>
 
                                         </CSSTransition>
-                                </TransitionGroup>} /> */}
-
-                                                        <Route path="/home" element={<Home />} />
-                                                        <Route path="/about" element={<About />} />
-                                                        <Route path="/work" element={<WorkExperience />} />
-                                                        <Route path="/projects" element={<Projects />} />
-                                                        <Route path="/contact" element={<Contact />} />
+                                </TransitionGroup>} />
                         
                         </Route>
                 </Routes>
